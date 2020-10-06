49c4f27b5919c267c95abc2d9f1a8bf8a485d58d
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-06 11:48:07 +0200

    init

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.awt.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String mark;
+    private String model;
+    private Color color;
+}
